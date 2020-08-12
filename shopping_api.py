feb6baab44b9af1947f5d8c54eccd349793890f7
MalarSankar <smalar1998@gmail.com>
2020-08-12 11:45:24 +0530

    quantity details added

--- shopping_api.py
+++ shopping_api.py
@@ -1,6 +1,5 @@
 from flask import Flask,request,jsonify
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 app=Flask(__name__)
 app.config['DEBUG']=True
@@ -11,58 +10,55 @@ def connect_db():
 
 @app.route('/')
 def home_page():
-    return "WELCOME  TO   ONLINE   SHOPPING"
-
-@app.route('/login',methods=['POST'])
-def login():
-    value = "invalid user"
-    user_name = request.form['user_name']
-    password = request.form['password']
-    password_list = db.execute("select * from users where password='{}' and name='{}'".format(password, user_name))
-    for row in password_list:
-        value = "welcome" + " " + str(row.name)
-    return jsonify(value)
-
-def category_format(row):
-    return   str(row.id) +  ": " + str(row.name)
+    return "WELCOME  TO   ONLINE   SHOPPING",200
 
 @app.route('/categories',methods=['GET'])
 def fetch_categories():
-        category_list = db.execute("select * from categories")
-        category= [category_format(row) for row in category_list]
-        return jsonify(category)
+    category_list = db.execute("select * from categories")
+    category=[dict(row) for row in category_list]
+    return jsonify(category),200
 
 @app.route('/categories/<category_id>' ,methods=['GET'])
 def fetch_items(category_id):
-    formated_item_list=[]
-    item_list = db.execute("select items.name,items.price ,sellers.name,sellers.phone_no from "
+    formated_items=[]
+    item_list = db.execute("select items.name,items.price ,sellers.name ,sellers.phone_no from "
                            "((items inner join categories on items.category_id=categories.id) "
                            "inner join sellers on items.seller_id=sellers.id) where categories.id='{}';".format(category_id))
-    for row in item_list:
-        formated_item_list.append("name:"+str(row[0])+"  "+"price:"+str(row[1])+"  "+"seller name:"+str(row[2])+"  "+"seller phone_no:"+str(row[3]))
-    return jsonify(formated_item_list)
+    for res in item_list:
+        formated_items.append ("name:{} price:{} sellername:{} phoneno:{}".format(res[0],res[1],res[2],res[3]))
+    return jsonify(formated_items)
+
 
 @app.route('/cart/add',methods=['POST'])
 def add_cart():
     user_id=request.form['user_id']
-    item=request.form['item']
-    db.execute("insert into carts(user_cart_id,item) values({},'{}');".format(user_id,item))
-    return "Item added successfully "
+    item_id=request.form['item_id']
+    db.execute("insert into carts(user_cart_id,item_id) values({},'{}');".format(user_id,item_id))
+    return "Item added successfully ",200
 
+@app.route('/cart/quantity',methods=['PUT'])
+def modify_quantity():
+    user_id = request.form['user_id']
+    item_id = request.form['item_id']
+    quantity=request.form['quantity']
+    db.execute("update carts set quantity={} where user_cart_id={} and item_id={};".format(quantity,user_id,item_id))
+    return "successfully modified",200
 
 @app.route('/cart/delete',methods=['DELETE'])
 def delete_item():
     user_id = request.form['user_id']
-    item = request.form['item']
-    db.execute("delete from carts where user_cart_id={} and item='{}';".format(user_id, item))
-    return "Item deleted successfully"
+    item_id = request.form['item_id']
+    db.execute("delete from carts where user_cart_id={} and item_id='{}';".format(user_id, item_id))
+    return "Item deleted successfully",200
 
-@app.route('/view cart',methods=['GET'])
+@app.route('/cart',methods=['GET'])
 def fetch_cart_items():
     user_id = request.form['user_id']
-    cart_items=db.execute("select item from carts where user_cart_id={} ;".format(user_id))
-    item=[str(row.item) for row in cart_items]
-    return jsonify(item)
+    cart_items=db.execute("select items.name from items inner join carts on items.id=carts.item_id "
+                          "inner join cart_id_holders on cart_id_holders.cart_id=carts.user_cart_id where cart_id_holders.user_id={} ;".format(user_id))
+    item=[str(row[0]) for row in cart_items]
+    return jsonify(item),200
+
 db=connect_db()
 if __name__ == "__main__":
     app.run()
