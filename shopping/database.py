dd2ceb347c777a0ca7b1360fef5b29b15639f868
MalarSankar <smalar1998@gmail.com>
2020-08-15 17:06:21 +0530

    remove folder

--- shopping/database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-
-def connect_db():
-    connection_string="postgresql://postgres:7538821247@localhost:5432/shopping"
-    return create_engine(connection_string)
-
