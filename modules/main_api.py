911a2180b094c5760c1305361e43951f3079c955
MalarSankar <smalar1998@gmail.com>
2020-08-31 07:39:28 +0530

    refactor works implement

--- modules/main_api.py
+++ modules/main_api.py
@@ -2,7 +2,6 @@ from flask import  Flask
 from login import login_blueprint
 from category import category_blueprint
 from cart import cart_blueprint
-from logout import logout_blueprint
 import os
 
 app=Flask(__name__,template_folder='template')
@@ -13,7 +12,7 @@ app.config['SECRET KEY']=os.environ.get('SECRET_KEY')
 app.register_blueprint(login_blueprint)
 app.register_blueprint(category_blueprint)
 app.register_blueprint(cart_blueprint)
-app.register_blueprint(logout_blueprint)
+
 
 if __name__ == "__main__":
     app.run(host="localhost",port=8000)
